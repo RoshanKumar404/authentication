399d436d53de538bbdc3dcc3995f95fbe4d6c3fe
RoshanKumar404 <roshankuar951@gmail.com>
2024-09-17 15:00:25 +0530

    setting coookie

---
+++ app.js
@@ -0,0 +1,22 @@
+const express=require('express')
+const cookieParser=require('cookie-parser')
+const app= express();
+app.use(cookieParser())
+
+app.get('/',function (req,res) {
+    res.cookie('name','babu')
+    res.send('tum sikh rhe ho')
+    console.log('cookie set ho gya hau');
+    
+    
+})
+app.get('/route1',function(req,res){
+    console.log(`cookie of route1 : `, req.cookies);
+    
+    res.send('jai ho baba ki')
+});
+
+app.listen(3222, function(){
+    console.log('your server is running on port 3222');
+    
+})
